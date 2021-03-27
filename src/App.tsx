79a953a796ae57316efbb7b84e882ae888a9c668
Vitor Rubim <vitorrubim1@hotmail.com>
2021-03-27 20:30:51 -0300

    chore: configurando a navegaçao do app com react-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import "react-native-gesture-handler"; // react-navigation, trabalhar com rotas, exige que seja na primeira linha do arquivo de inicio
+
 import React from "react";
 import { View, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native"; /*
+  NavigationContainer: é o provider do contexto de navegação da aplicação.
+  ele tem que estar por volta de tudo, pra que a navegação/rota funcione;
+*/
+
+import Routes from "./routes";
 
 const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ flex: 1, backgroundColor: "#312e38" }} />
-    </>
+      <View style={{ flex: 1, backgroundColor: "#312e38" }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
   );
 };
 
