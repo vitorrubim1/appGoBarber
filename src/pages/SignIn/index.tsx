891d0e44924c98dbb8a91ce47add7082022586b4
Vitor Rubim <vitorrubim1@hotmail.com>
2021-03-28 12:32:47 -0300

    chore: finalizando a estrutura e estilizaçao da tela de autenticaçao do aplicativo

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,60 @@
 import React from "react";
-import { Image } from "react-native";
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import Icon from "react-native-vector-icons/Feather";
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 import logoImage from "../../assets/logo.png";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountText,
+} from "./styles";
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImage} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        enabled
+      >
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <Image source={logoImage} />
 
-      <Title>Faça seu Login</Title>
+            <View>
+              <Title>Faça seu Login</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="Digite seu email" />
-      <Input name="password" icon="lock" placeholder="Digite sua senha" />
+            <Input name="email" icon="mail" placeholder="Digite seu email" />
+            <Input name="password" icon="lock" placeholder="Digite sua senha" />
 
-      <Button onPress={() => {}}>Entrar</Button>
-    </Container>
+            <Button onPress={() => {}}>Entrar</Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText> Esqueci minha senha </ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountText>Criar uma conta</CreateAccountText>
+      </CreateAccountButton>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -11,7 +12,38 @@ export const Container = styled.View`
 export const Title = styled.Text`
   font-size: 24px;
   color: #f4ede8;
-  font-family: 'RobotoSlab-Medium';
+  font-family: "RobotoSlab-Medium";
 
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-family: "RobotoSlab-Regular";
+  font-size: 16px;
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 10px 0 ${10 + getBottomSpace()}px;
+
+  background: #111111;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CreateAccountText = styled.Text`
+  color: #ff9000;
+  font-family: "RobotoSlab-Regular";
+  font-size: 16px;
+  margin-left: 16px;
 `;
